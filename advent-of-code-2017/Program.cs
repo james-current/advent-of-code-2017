1a4b053ab795cb168f676dd33a18fcdf5de5b5c1
James Current <james.current@daugherty.com>
2017-12-04 14:50:10 -0600

    Creating project and solving problem 1

---
+++ advent-of-code-2017/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace advent_of_code_2017
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sum = 0;
+            var captcha = args[0].ToCharArray();
+            for (var i = 0; i < captcha.Length; i++)
+            {
+                var next = i == captcha.Length - 1 ? 0 : i + 1;
+                if (captcha[i] == captcha[next])
+                {
+                    sum += int.Parse(captcha[i].ToString());
+                }
+            }
+            Console.WriteLine($"Sum is: {sum}");
+        }
+    }
+}
