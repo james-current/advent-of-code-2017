0173de7bcb091ad2212cc48d173b7a64fa40ad25
James Current <james.current@daugherty.com>
2017-12-05 15:14:08 -0600

    Stubbed out Problem 2 and adjusted for reusing code

--- advent-of-code-2017/Test/Day2Tests.cs
+++ advent-of-code-2017/Test/Day2Tests.cs
@@ -8,28 +8,28 @@ namespace Test
         [Fact]
         public void TestProcessRow5195Problem1()
         {
-            var test = Day2.ProcessRow("5 1 9 5");
+            var test = Day2.ProcessRow1("5 1 9 5");
             Assert.Equal(8, test);
         }
 
         [Fact]
         public void TestProcessRow753Problem1()
         {
-            var test = Day2.ProcessRow("7 5 3");
+            var test = Day2.ProcessRow1("7 5 3");
             Assert.Equal(4, test);
         }
 
         [Fact]
         public void TestProcessRow2468Problem1()
         {
-            var test = Day2.ProcessRow("2 4 6 8");
+            var test = Day2.ProcessRow1("2 4 6 8");
             Assert.Equal(6, test);
         }
 
         [Fact]
         public void TestProcessRowTabsProblem1()
         {
-            var test = Day2.ProcessRow("5\t7\t2");
+            var test = Day2.ProcessRow1("5\t7\t2");
             Assert.Equal(5, test);
         }
 
@@ -39,6 +39,35 @@ namespace Test
             var input = "5 1 9 5\n7 5 3\n2 4 6 8";
             var test = Day2.Problem1(input);
             Assert.Equal(18, test);
+        }
+
+        [Fact]
+        public void TestProcessRow5928Problem2()
+        {
+            var test = Day2.ProcessRow2("5 9 2 8");
+            Assert.Equal(4, test);
+        }
+
+        [Fact]
+        public void TestProcessRow9473Problem2()
+        {
+            var test = Day2.ProcessRow2("9 4 7 3");
+            Assert.Equal(3, test);
+        }
+
+        [Fact]
+        public void TestProcessRow3865Problem2()
+        {
+            var test = Day2.ProcessRow2("3 8 6 5");
+            Assert.Equal(2, test);
+        }
+
+        [Fact]
+        public void TestProblem2Rows()
+        {
+            var input = "5 9 2 8\n9 4 7 3\n3 8 6 5";
+            var test = Day2.Problem2(input);
+            Assert.Equal(9, test);
         }
     }
 }
--- advent-of-code-2017/advent/Day2.cs
+++ advent-of-code-2017/advent/Day2.cs
@@ -7,19 +7,41 @@ namespace advent
     {
         public static int Problem1(string input)
         {
-            var regex = new Regex(@"\n");
-            return regex.Split(input)
-                .Select(ProcessRow)
+            return SplitTable(input)
+                .Select(ProcessRow1)
                 .Sum();
         }
 
-        public static int ProcessRow(string row)
+        public static int Problem2(string input)
         {
-            var regex = new Regex(@"\s");
-            var nums = regex.Split(row).Select(int.Parse).ToArray();
+            var sum = 0;
+            return sum;
+        }
+
+        private static string[] SplitTable(string input)
+        {
+            var regex = new Regex(@"\n");
+            return regex.Split(input);
+        }
+
+        public static int ProcessRow1(string row)
+        {
+            var nums = SplitRow(row);
             var max = nums.Max();
             var min = nums.Min();
             return max - min;
+        }
+
+        public static int ProcessRow2(string row)
+        {
+            var sum = 0;
+            return sum;
+        }
+
+        private static int[] SplitRow(string row)
+        {
+            var regex = new Regex(@"\s");
+            return regex.Split(row).Select(int.Parse).ToArray();
         }
     }
 }
