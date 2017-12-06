46dc17ff9a090ae8e13c4aa2e83199ac478308fd
James Current <james.current@daugherty.com>
2017-12-06 10:32:57 -0600

    Setup for Day3 Problem1

---
+++ advent-of-code-2017/Test/Day3Tests.cs
@@ -0,0 +1,36 @@
+﻿using advent;
+using Xunit;
+
+namespace Test
+{
+    public class Day3Tests
+    {
+        [Fact]
+        public void Test1Problem1()
+        {
+            var test = Day3.Problem1(1);
+            Assert.Equal(0, test);
+        }
+
+        [Fact]
+        public void Test12Problem1()
+        {
+            var test = Day3.Problem1(12);
+            Assert.Equal(3, test);
+        }
+
+        [Fact]
+        public void Test23Problem1()
+        {
+            var test = Day3.Problem1(23);
+            Assert.Equal(2, test);
+        }
+
+        [Fact]
+        public void Test1024Problem1()
+        {
+            var test = Day3.Problem1(1024);
+            Assert.Equal(31, test);
+        }
+    }
+}
---
+++ advent-of-code-2017/advent/Day3.cs
@@ -0,0 +1,15 @@
+﻿namespace advent
+{
+    public static class Day3
+    {
+        public static int Problem1(int address)
+        {
+            if (address == 1)
+            {
+                return 0;
+            }
+
+            return -1;
+        }
+    }
+}
