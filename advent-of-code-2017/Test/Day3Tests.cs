e6b6dd87fb20579a65278293504eb09f36ec8915
James Current <james.current@daugherty.com>
2017-12-06 14:55:16 -0600

    Solved Day3 Problem 1

--- advent-of-code-2017/Test/Day3Tests.cs
+++ advent-of-code-2017/Test/Day3Tests.cs
@@ -5,11 +5,27 @@ namespace Test
 {
     public class Day3Tests
     {
+        const int real = 289326;
+
+        [Fact]
+        public void Test1CalculateLocation()
+        {
+            var test = Day3.CalculateLocation(1);
+            Assert.Equal((0, 0), test);
+        }
+
         [Fact]
         public void Test1Problem1()
         {
             var test = Day3.Problem1(1);
             Assert.Equal(0, test);
+        }
+
+        [Fact]
+        public void Test12CalculateLocation()
+        {
+            var test = Day3.CalculateLocation(12);
+            Assert.Equal((2, 1), test);
         }
 
         [Fact]
@@ -31,6 +47,13 @@ namespace Test
         {
             var test = Day3.Problem1(1024);
             Assert.Equal(31, test);
+        }
+
+        [Fact]
+        public void TestRealProblem1()
+        {
+            var test = Day3.Problem1(real);
+            Assert.Equal(419, test);
         }
     }
 }
--- advent-of-code-2017/advent/Day3.cs
+++ advent-of-code-2017/advent/Day3.cs
@@ -1,15 +1,48 @@
-﻿namespace advent
+﻿using System;
+using System.ComponentModel;
+
+namespace advent
 {
     public static class Day3
     {
         public static int Problem1(int address)
         {
+            (int layer, int stepsToCenter) location = CalculateLocation(address);
+
+            return location.layer + location.stepsToCenter;
+        }
+
+        public static ValueTuple<int, int> CalculateLocation(int address)
+        {
+            var location = (layer: 0, stepsToCenter: 0);
             if (address == 1)
             {
-                return 0;
+                return location;
             }
+            var sideSize = 1;
+            for (var memAddress = 1; memAddress <= address; location.layer++, sideSize += 2)
+            {
 
-            return -1;
+                var center = sideSize / 2;
+
+                for (var side = 0; side < 4; side++)
+                {
+                    if (memAddress == 1)
+                    {
+                        memAddress++;
+                        break;
+                    }
+                    for (var sidePosition = 1; sidePosition < sideSize && memAddress <= address; sidePosition++, memAddress++)
+                    {
+                        location.stepsToCenter = Math.Abs(sidePosition - center);
+                        if (memAddress == address)
+                        {
+                            return location;
+                        }
+                    }
+                }
+            }
+            return location;
         }
     }
 }
