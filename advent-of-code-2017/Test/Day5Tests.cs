8f84c7b1615b18687681f9b753e0f1539f53fc10
James Current <james.current@daugherty.com>
2017-12-08 11:28:52 -0600

    Day5 Problem1 tests passing

---
+++ advent-of-code-2017/Test/Day5Tests.cs
@@ -0,0 +1,15 @@
+﻿using advent;
+using Xunit;
+
+namespace Test
+{
+    public class Day5Tests
+    {
+        [Fact]
+        public void TestProblem1()
+        {
+            var test = Day5.Problem1("0\n3\n0\n1\n-3");
+            Assert.Equal(5, test);
+        }
+    }
+}
---
+++ advent-of-code-2017/advent/Day5.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace advent
+{
+    public static class Day5
+    {
+        public static int Problem1(string jumpList)
+        {
+            var regex = new Regex(@"\n");
+            var jumps = regex.Split(jumpList).Select(int.Parse).ToArray();
+            var steps = 0;
+            var i = 0;
+            while (true)
+            {
+                steps++;
+                var next = jumps[i]++ + i;
+
+                if (next < 0 || next > jumps.Length - 1)
+                {
+                    return steps;
+                }
+
+                i = next;
+            }
+        }
+    }
+}
