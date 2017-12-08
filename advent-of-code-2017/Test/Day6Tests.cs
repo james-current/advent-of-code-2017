064af3d63abe267abc6c07229d9dd3f7d6aa60ce
James Current <james.current@daugherty.com>
2017-12-08 15:50:47 -0600

    Day6 Problem1 complete

---
+++ advent-of-code-2017/Test/Day6Tests.cs
@@ -0,0 +1,24 @@
+﻿using advent;
+using Xunit;
+
+namespace Test
+{
+    public class Day6Tests
+    {
+        private const string Real = @"0	5	10	0	11	14	13	4	11	8	8	7	1	4	12	11";
+        
+        [Fact]
+        public void TestProblem1()
+        {
+            var test = Day6.Problem1(@"0	2	7	0");
+            Assert.Equal(5, test);
+        }
+
+        [Fact]
+        public void TestProblem1Real()
+        {
+            var test = Day6.Problem1(Real);
+            Assert.Equal(7864, test);
+        }
+    }
+}
---
+++ advent-of-code-2017/advent/Day6.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace advent
+{
+    public static class Day6
+    {
+        public static int Problem1(string input)
+        {
+            var memBanks = ParseInput(input);
+            var seen = new HashSet<int[]>(new IntArrayEqualityComparer()) {(int[]) memBanks.Clone()};
+            var steps = 0;
+
+            while (true)
+            {
+                steps++;
+                var max = memBanks.Max();
+                var indexOfMax = Array.IndexOf(memBanks, max);
+                memBanks[indexOfMax] = 0;
+
+                var startIndex = indexOfMax == memBanks.Length - 1 ? 0 : indexOfMax + 1;
+
+                for (var i = startIndex; max > 0; max--)
+                {
+                    memBanks[i]++;
+
+                    if (++i == memBanks.Length)
+                    {
+                        i = 0;
+                    }
+                }
+
+                if (seen.Contains(memBanks))
+                {
+                    return steps;
+                }
+
+                seen.Add((int[]) memBanks.Clone());
+            }
+        }
+
+        private static int[] ParseInput(string input)
+        {
+            var regex = new Regex(@"\s");
+            return regex.Split(input).Select(int.Parse).ToArray();
+        }
+    }
+
+    internal class IntArrayEqualityComparer : IEqualityComparer<int[]>
+    {
+        public bool Equals(int[] x, int[] y)
+        {
+            if (x == null)
+            {
+                return y == null;
+            }
+            return !x.Where((t, i) => t != y[i]).Any();
+        }
+
+        public int GetHashCode(int[] obj)
+        {
+            return obj.Aggregate(17, (current, i) => current * 23 + i);
+        }
+    }
+}
