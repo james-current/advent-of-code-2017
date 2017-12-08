664d365581c39229a481feeb47427b464c8c7825
James Current <james.current@daugherty.com>
2017-12-08 09:45:22 -0600

    Stubs for Day4 Problem1

---
+++ advent-of-code-2017/Test/Day4Tests.cs
@@ -0,0 +1,29 @@
+﻿using advent;
+using Xunit;
+
+namespace Test
+{
+    public class Day4Tests
+    {
+        [Fact]
+        public void TestIsValidPassphraseValid()
+        {
+            var test = Day4.IsValidPassphrase("aa bb cc dd ee");
+            Assert.Equal(true, test);
+        }
+
+        [Fact]
+        public void TestIsValidPassphraseInvalid()
+        {
+            var test = Day4.IsValidPassphrase("aa bb cc dd aa");
+            Assert.Equal(false, test);
+        }
+
+        [Fact]
+        public void TestIsValidPassphraseWordContainsAnotherWord()
+        {
+            var test = Day4.IsValidPassphrase("aa bb cc dd aaa");
+            Assert.Equal(true, test);
+        }
+    }
+}
---
+++ advent-of-code-2017/advent/Day4.cs
@@ -0,0 +1,15 @@
+﻿namespace advent
+{
+    public class Day4
+    {
+        public static int Problem1(string passphrases)
+        {
+            return -1;
+        }
+
+        public static bool IsValidPassphrase(string passphrase)
+        {
+            return false;
+        }
+    }
+}
